68e561d6d9374b9040f12a43b878b907a81ec40f
Lyubitshka <antwarowska@gmail.com>
2023-10-27 13:52:37 +0200

    animatios

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -28,24 +28,37 @@ class MealDetailsScreen extends ConsumerWidget {
                   .read(favoriteMealsProvider.notifier)
                   .toggleFavoriteStatus(meal);
               ScaffoldMessenger.of(context).clearMaterialBanners();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(result ? 'Meal added' : 'Meal removed'),
-                ),
-              );
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text(result ? 'Meal added' : 'Meal removed'),
+              ));
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 420),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween(begin: 0.5, end: 1.0).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           )
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(
               height: 14,
